673df9e0d82f8b4d3559662b7b30639f4f0b128a
Drew Showalter <maelstroswim@gmail.com>
2016-04-23 18:26:43 +1000

    Test homepage with handy-dandy jquery.

--- routes/schema.js
+++ routes/schema.js
@@ -2,9 +2,13 @@ const schema = require('root/schema');
 
 module.exports = {
   http: app => {
+    app.set('views', __dirname + '/views');
     app.get('/schema', (req, res) => {
       res.json(schema);
     });
+    app.get('/', (req, res) => {
+      res.render('index');
+    })
   },
   ws: () => {}
 };
