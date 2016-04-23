a59454dd11d9d639a133bb34ceb045ff413ba484
Drew Showalter <maelstroswim@gmail.com>
2016-04-23 18:29:01 +1000

    Actually renders the index.

--- routes/schema.js
+++ routes/schema.js
@@ -2,12 +2,11 @@ const schema = require('root/schema');
 
 module.exports = {
   http: app => {
-    app.set('views', __dirname + '/views');
     app.get('/schema', (req, res) => {
       res.json(schema);
     });
     app.get('/', (req, res) => {
-      res.render('index');
+      res.render('views/index.html');
     })
   },
   ws: () => {}
